15d9edd23b18f9943bf03cfc559b5e5d3e0497ac
phamvankhoa <phamvankhoa.150600@gmail.com>
2021-11-30 21:39:50 +0700

    first commit

---
+++ src/NoCashAvailableException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Pham Tuan
+ */
+class NoCashAvailableException extends Exception {
+    //https://github.com/UB-GEI-SD/Examples/tree/master/RMI
+    
+}
